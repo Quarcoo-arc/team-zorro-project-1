9a4525314af24d4b9c853bb029329eb98edcf12c
akanniquadree <78854242+akanniquadree@users.noreply.github.com>
2021-11-06 17:05:35 +0100

    header component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from "./header";
 
 function App() {
-  return <div className="App">Hello React!</div>;
+  return <div className="App">
+    Hello React!
+    <Header/>
+    </div>;
 }
 
 export default App;
