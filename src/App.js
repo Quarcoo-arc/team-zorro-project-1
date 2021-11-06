63d74f6458a2f8962f4c903a0eb94bd55173978f
Michael Quarcoo <michaelquarcoo04@gmail.com>
2021-11-06 18:06:49 +0000

    SearchBar component - Minor Fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
+import Header from "./Header";
 import SearchBar from "./Components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <div>
+        <Header />
         <SearchBar />
       </div>
     </div>
   );
-=======
-import Header from "./header";
-
-function App() {
-  return <div className="App">
-    Hello React!
-    <Header/>
-    </div>;
->>>>>>> 9a4525314af24d4b9c853bb029329eb98edcf12c
 }
 
 export default App;
